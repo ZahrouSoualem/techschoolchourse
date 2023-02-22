fe27a1d33ded7881d4f21ace25ca52ef4106c3c0
ZahrouSoualem <81196457+ZahrouSoualem@users.noreply.github.com>
2023-02-22 15:37:31 +0100

    apimigration

--- api/account.go
+++ api/account.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"fmt"
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,30 +10,26 @@ import (
 
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required" `
-	Currency string `json:"currency" binding:"required, oneof:USD EUR" `
+	Currency string `json:"currency" binding:"required" `
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
 
 	var req createAccountRequest
-	fmt.Println("Hello World")
+
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResqponse(err))
 		return
 	}
-	fmt.Println("Hello World 2")
-	fmt.Println(req.Owner)
+
 	args := db.CreateAuthorParams{
 		Owner:    req.Owner,
 		Currency: req.Currency,
 		Balance:  0,
 	}
-	fmt.Println("Hello World 3")
-	fmt.Println(req.Owner)
+
 	account, err := server.store.CreateAuthor(ctx, args)
 
-	fmt.Println("Hello World 2")
-	fmt.Println(account.Currency)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResqponse(err))
 		return
@@ -41,4 +37,57 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, account)
 
+}
+
+type getAccountParams struct {
+	ID int64 `uri:"id" binding:"required,min=1" `
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountParams
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResqponse(err))
+		return
+	}
+
+	account, err := server.store.GetAuthor(ctx, req.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResqponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResqponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountParams struct {
+	pageID   int32 `form:"page_id" binding:"required,min=1" `
+	pageSize int32 `form:"page_size" binding:"required , min=5 , max=10" `
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountParams
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResqponse(err))
+		return
+	}
+
+	args := db.ListAuthorsParams{
+		Limit:  req.pageSize,
+		Offset: (req.pageID - 1) * req.pageSize,
+	}
+	accounts, err := server.store.ListAuthors(ctx, args)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResqponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
--- api/server.go
+++ api/server.go
@@ -18,6 +18,8 @@ func NewServer(store *db.Store) *Server {
 	// add route to this router
 
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
 	server.router = router
 
 	return server
